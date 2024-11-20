3cde3e275aa9ec381ab9a0696b9769b9190e5bd7
Jose <mortox1990@gmail.com>
2024-11-20 20:52:20 +0100

    Dando los ultimos retoques

--- Principal.py
+++ Principal.py
@@ -1,7 +1,8 @@
 import tkinter as tk
 from tkinter import messagebox
-import random
 import mysql.connector
+import pip
+from PIL import Image, ImageTk
 
 from Conexion import conectar_db
 
@@ -75,8 +76,22 @@ def iniciar_juego(categoria):
 # Función para verificar la letra ingresada
 def verificar_letra():
     global intentos
-    letra = entry_letra.get().lower()
+    letra = entry_letra.get()
     entry_letra.delete(0, tk.END)
+    label_imagen = tk.Label(frame2, bg="#4E7C7E")
+    label_imagen.pack(pady=10)
+
+    #PROBLEMAS CON LAS IMAGENES
+    imagenes = []
+    for i in range(7):
+        try:
+            img = Image.open(f"imagenes/Ahorcado0{i}.png")  # Ruta de las imágenes
+            img = img.resize((150, 150))  # Redimensionar la imagen
+            imagenes.append(ImageTk.PhotoImage(img))
+        except FileNotFoundError:
+            print(f"Advertencia: No se encontró la imagen imagenes/Ahorcado_0{i}.png")
+            imagenes.append(None)
+
 
     if len(letra) != 1 or not letra.isalpha():
         messagebox.showerror("Error", "Por favor, ingresa una sola letra válida.")
@@ -90,6 +105,20 @@ def verificar_letra():
 
     if letra not in palabra:
         intentos += 1
+        label_imagen.config(image=imagenes[intentos]) # Actualiza la imagen
+
+    actualizar_tablero()
+
+    if intentos == 6:
+        messagebox.showinfo("Derrota", f"Perdiste. La palabra era: {palabra}")
+        actualizar_estadisticas(nombre_jugador, False)
+        reiniciar_juego()
+    elif all(letra in letras_adivinadas for letra in palabra):
+        messagebox.showinfo("Victoria", "Has adivinado la palabra.")
+        actualizar_estadisticas(nombre_jugador, True)
+        reiniciar_juego()
+
+
 
     actualizar_tablero()
 
@@ -132,6 +161,8 @@ label_nombre.pack(pady=5)
 entry_nombre = tk.Entry(frame1, font=("Arial", 12))
 entry_nombre.pack(pady=5)
 
+
+#metodo para iniciar
 def comenzar_juego(categoria):
     global nombre_jugador
     nombre_jugador = entry_nombre.get().strip()
@@ -144,20 +175,24 @@ def comenzar_juego(categoria):
 label_categoria = tk.Label(frame1, text="Elije una categoría", bg="#4E7C7E", fg="black", font=("Arial", 12))
 label_categoria.pack(pady=10)
 
-boton_fruta = tk.Button(frame1, text="Frutas", bg="#4FC3F7", command=lambda: comenzar_juego("Frutas"))
+boton_fruta = tk.Button(frame1, text="Frutas", bg="#4FC3F7",width=18, height=1, command=lambda: comenzar_juego("Frutas"))
 boton_fruta.pack(pady=5)
 
-boton_termino = tk.Button(frame1, text="Conceptos Informáticos", bg="#4FC3F7", command=lambda: comenzar_juego("Conceptos Informáticos"))
+boton_termino = tk.Button(frame1, text="Conceptos Informáticos", bg="#4FC3F7",width=18, height=1, command=lambda: comenzar_juego("Conceptos Informáticos"))
 boton_termino.pack(pady=5)
 
-boton_nombres = tk.Button(frame1, text="Nombres", bg="#4FC3F7", command=lambda: comenzar_juego("Nombres"))
+boton_nombres = tk.Button(frame1, text="Nombres", bg="#4FC3F7",width=18, height=1, command=lambda: comenzar_juego("Nombres"))
 boton_nombres.pack(pady=5)
 
-boton_salir = tk.Button(frame1, text="Salir", bg="#4FC3F7", command=ventana.quit)
+boton_salir = tk.Button(frame1, text="Salir", bg="#4FC3F7",width=18, height=1, command=ventana.quit)
 boton_salir.pack(pady=10)
 
 # Frame del juego (Frame 2)
 frame2 = tk.Frame(ventana, bg="#4E7C7E")
+
+# Frame para mostrar la imagen
+label_imagen = tk.Label(frame2, bg="#4E7C7E", )
+label_imagen.pack(pady=10)
 
 label_titulo_juego = tk.Label(frame2, text="El Ahorcado", font=("Arial", 24, "bold"), bg="#4E7C7E", fg="black")
 label_titulo_juego.pack(pady=10)
@@ -168,13 +203,13 @@ label_tablero.pack(pady=20)
 entry_letra = tk.Entry(frame2, font=("Arial", 12))
 entry_letra.pack(pady=5)
 
-boton_verificar = tk.Button(frame2, text="Verificar Letra", bg="#4FC3F7", command=verificar_letra)
+boton_verificar = tk.Button(frame2, text="Verificar Letra", bg="#4FC3F7",width=18, height=1, command=verificar_letra)
 boton_verificar.pack(pady=10)
 
 label_intentos = tk.Label(frame2, text="Intentos: 0/6", bg="#4E7C7E", fg="white", font=("Arial", 12))
 label_intentos.pack(pady=10)
 
-boton_volver = tk.Button(frame2, text="Volver", bg="#4FC3F7", command=reiniciar_juego)
+boton_volver = tk.Button(frame2, text="Volver", bg="#4FC3F7", width=18, height=1, command=reiniciar_juego)
 boton_volver.pack(pady=10)
 
 ventana.mainloop()
